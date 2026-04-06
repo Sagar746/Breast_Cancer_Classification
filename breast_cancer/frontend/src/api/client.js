fa3b231505a22dce9186aac674d1dfae8c7de941
Otshal Puri <itsmeotshal@gmail.com>
2026-04-06 18:50:36 +0300

    axios interceptors and auth api client

---
+++ breast_cancer/frontend/src/api/client.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://localhost:8000",
+  headers: { "Content-Type": "application/json" },
+});
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+api.interceptors.response.use(
+  (r) => r,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
+export const authApi = {
+  login: (email, password) =>
+    api.post("/auth/login",
+      new URLSearchParams({ username: email, password }),
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    ),
+  me: () => api.get("/auth/me"),
+};
+export default api;
+
