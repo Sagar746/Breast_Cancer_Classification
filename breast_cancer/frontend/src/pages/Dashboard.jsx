b9dde614358fc254a7c62a7a5f167ce6c7216054
Otshal Puri <itsmeotshal@gmail.com>
2026-04-29 00:09:10 +0300

    dashboard style change

--- breast_cancer/frontend/src/pages/Dashboard.jsx
+++ breast_cancer/frontend/src/pages/Dashboard.jsx
@@ -119,7 +119,7 @@ export default function Dashboard() {
           <div className="flex justify-between items-center py-4">
             <div>
               <h1 className="text-2xl font-bold text-gray-900">Dashboard</h1>
-              <p className="text-sm text-gray-600">Welcome !!!!, {user?.full_name}</p>
+              <p className="text-sm text-gray-600"> {user?.full_name}</p>
             </div>
             <button
               onClick={handleLogout}
