bbca74bc03b1e26f05acf48d27ecc312ebee1325
Sagar746 <tiwarisagar389@gmail.com>
2026-04-30 08:48:21 +0300

    importing patient api

--- breast_cancer/backend/app/main.py
+++ breast_cancer/backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.api.auth import router as auth_router
+from app.api.patients import router as patients_router
 
 
 @asynccontextmanager
@@ -25,6 +26,7 @@ app.add_middleware(
 )
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(patients_router, prefix="/patients", tags=["Patients"])
 
 @app.get("/health")
 async def health():
